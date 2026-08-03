cf0e18841f55767b756307cac3b689e15a08b628
aurrego7 <118835961+aurrego7@users.noreply.github.com>
2026-08-03 12:58:25 +0200

    Add missing prompts.py file from previous commit

---
+++ app/core/prompts.py
@@ -0,0 +1,12 @@
+rag_question_prompt = """
+Based on the following context, answer the question.
+If the answer is not in the context, say "I don't have enough information."
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
